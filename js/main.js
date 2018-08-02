1693b198a1a6d07fcc5094fe88cca2467765ebf5
Alexander <sigmanor@gmail.com>
2018-08-02 22:01:57 +0300

    Removed google analytics

--- js/main.js
+++ js/main.js
@@ -16,13 +16,13 @@ if (!!navigator.userAgent.match(/Trident\/7\./))
 
 /* Randomize background */
 var images = [
-'../img/bg1.png', 
-'../img/bg2.jpg', 
-'../img/bg3.jpg', 
-'../img/bg4.png', 
-'../img/bg5.png', 
-'../img/bg6.png', 
-'../img/bg7.png', 
+'../img/bg1.png',
+'../img/bg2.jpg',
+'../img/bg3.jpg',
+'../img/bg4.png',
+'../img/bg5.png',
+'../img/bg6.png',
+'../img/bg7.png',
 '../img/bg8.png'
 ];
 $('#main-page').css({'background-image': 'url(../img/' + images[Math.floor(Math.random()*images.length)] + ')'});
@@ -54,23 +54,6 @@ $('#myModal').bind('show.bs.modal', function () {
     $("html").css("margin-right", "0px");
 });
 /*modal*/
-
-/*google analytics*/
-(function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-ga('create', 'UA-43330640-3', 'auto');
-ga('send', 'pageview');
-/*google analytics*/
 
 /*modal download*/
 if (window.location.hash == "#more") {
